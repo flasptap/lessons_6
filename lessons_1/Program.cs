96f1013977df8d5724f6db9048e010e9fa494de0
Max <mrbroo123@gmail.com>
2023-07-22 20:06:42 +0300

    sohr

---
+++ lessons_1/Program.cs
@@ -0,0 +1,23 @@
+﻿string inputStr = ReadString("Введите числа через зяпятую: ");
+int count = 0;
+string tmpStr = string.Empty;
+
+for (int i = 0; i < inputStr.Length; i++)
+{
+    if (inputStr[i] != ',') tmpStr += inputStr[i];
+    else
+    {
+        if (Convert.ToInt32(tmpStr) > 0) count++;
+        tmpStr = string.Empty;
+    }
+}
+if (Convert.ToInt32(tmpStr) > 0) count++;
+
+Console.WriteLine($"Введено {count} чисел больше 0.");
+
+// Функция ввода ( строки )
+string ReadString(string message)
+{
+    Console.Write(message);
+    return Console.ReadLine();
+}
---
+++ lessons_2/Program.cs
@@ -0,0 +1,20 @@
+﻿double b1 = ReadTerminal("Введите b1: ");
+double k1 = ReadTerminal("Введите k1: ");
+double b2 = ReadTerminal("Введите b2: ");
+double k2 = ReadTerminal("Введите k2: ");
+ 
+double x = -(b1 - b2) / (k1 - k2);
+double y = k1 * x + b1;
+ 
+x = Math.Round(x, 3);
+y = Math.Round(y, 3);
+ 
+Console.WriteLine($"b1 = {b1},k1 = {k1},b2 = {b2},k2 = {k2} -> ({x};{y})");
+
+
+// Функция ввода ( строки )
+double ReadTerminal(string msg)
+{
+    Console.Write(msg);
+    return Convert.ToDouble(Console.ReadLine());
+}
